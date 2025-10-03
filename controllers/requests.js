829a8f0cf4e7db888a684d146a193bfc15f5e702
Daniel <dk807@live.mdx.ac.uk>
2025-10-03 01:43:40 +0100

    copied the needed files, functions and data from week 5a

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,20 +1,51 @@
 const path = require('path');
 const fs = require('fs');
+const url = require('url');
+
+const lodash = require('lodash');
+
+const lessons = require('../data/lessons');
+
 
 exports.getLogs = (req, res, next) => {
     console.log('Request URL: ' + req.url);
+    console.log("Request IP: " + req.ip);
+    console.log(['::ffff:127.0.0.1'].indexOf(req.ip));
     next();
 }
 
-exports.getRandomDecision = (req, res, next) => {
-    const randNum = 0.5 - Math.random();
+exports.getLessonLocation = (req, res) => {
+    const reqUrl = req.url;
+    const parsedUrl = url.parse(reqUrl, true);
+    const location = parsedUrl.query.location;
 
-    if (randNum > 0) {
-        return next();
-    }
-    const err = new Error('Bad luck. Try next time');
-    err.statusCode = 404;
-    next(err);
+
+    // I want to create a new array and in each element (which each element 
+    // is an object) and i want to only pick some of the properties
+
+    // const locations = lodash.map(lessons.lessons, function (elm) {
+    //     return lodash.pick(elm, ['location']);
+    // });
+    // console.log(locations)
+    // const lesson = lodash.find(locations, lesson => lesson.location === location);
+    const lesson = lodash.find(lessons.lessons, lesson => lesson.location === location);
+    res.end(JSON.stringify(lesson));
+}
+
+exports.getFirstPrice = (req, res) => {
+    // const lesson = lodash.filter(lessons.lessons, lesson => lesson.price < 100); // Returns array of lessons
+    const lesson = lodash.find(lessons.lessons, lesson => lesson.price < 100);
+    res.end(JSON.stringify(lesson));
+}
+
+exports.getLastPrice = (req, res) => {
+    const lesson = lodash.findLast(lessons.lessons, lesson => lesson.price < 100);
+    res.end(JSON.stringify(lesson));
+}
+
+exports.getSuffledLessons = (req, res) => {
+    const shuffled = lodash.shuffle(lessons.lessons);
+    res.end(JSON.stringify(shuffled));
 }
 
 exports.getFile = (req, res, next) => {
